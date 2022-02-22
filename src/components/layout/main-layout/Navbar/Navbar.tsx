b827dd3ec5097e222def82c4f3c3973344c05d82
AkromDev <akromsprinter@gmail.com>
2022-02-22 22:14:00 +0900

    modify navbar for mobile

--- src/components/layout/main-layout/Navbar/Navbar.tsx
+++ src/components/layout/main-layout/Navbar/Navbar.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { ScrollArea, DEFAULT_THEME } from '@mantine/core';
-import { HomeIcon, DashboardIcon } from '@modulz/radix-icons';
+import { HomeIcon, DashboardIcon, PersonIcon } from '@modulz/radix-icons';
+import { SignInIcon } from '@primer/octicons-react';
+import { useAuthUser, withAuthUser } from 'next-firebase-auth';
 import NavbarMainLink from './NavbarMainLink';
 import useStyles from './Navbar.styles';
 
-const links = [
+const loggedinUsers = [
   {
     to: '/',
     label: 'Home',
     theme: DEFAULT_THEME.colors.blue[6],
     icon: HomeIcon,
   },
+  {
+    to: '/profile',
+    label: 'Profile',
+    theme: DEFAULT_THEME.colors.blue[6],
+    icon: PersonIcon,
+  },
+];
+
+const unLoggedUsers = [
+  {
+    to: '/',
+    label: 'Home',
+    theme: DEFAULT_THEME.colors.blue[6],
+    icon: HomeIcon,
+  },
+  {
+    to: '/auth',
+    label: 'Register',
+    theme: DEFAULT_THEME.colors.blue[6],
+    icon: SignInIcon,
+  },
+];
+
+const adminLinks = [
+  ...loggedinUsers,
   {
     to: '/admin',
     label: 'Admin',
@@ -19,13 +46,25 @@ const links = [
   },
 ];
 
+function getLinks(user) {
+  if (!user) {
+    return unLoggedUsers;
+  }
+  if (user.claims.role === 'Manager') {
+    return adminLinks;
+  }
+
+  return loggedinUsers;
+}
 interface NavbarProps {
   opened: boolean;
   onClose(): void;
 }
 
-export default function Navbar({ opened, onClose }: NavbarProps) {
+function Navbar({ opened, onClose }: NavbarProps) {
   const { classes, cx } = useStyles();
+  const authUser = useAuthUser();
+  const links = getLinks(authUser);
 
   const main = links.map((item) => (
     <NavbarMainLink
@@ -47,3 +86,5 @@ export default function Navbar({ opened, onClose }: NavbarProps) {
     </nav>
   );
 }
+
+export default withAuthUser<any>()(Navbar);
