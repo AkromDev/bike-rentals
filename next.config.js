498abebf55a2caed4c7099a285d7301f6a69b8b7
AkromDev <akromsprinter@gmail.com>
2022-02-22 17:38:42 +0900

    ignore ts errors on deploy

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = withBundleAnalyzer({
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
